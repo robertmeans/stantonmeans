52f1e7eb123ca32eba8225ab3463de2764b94925
robertmeans <robert@robertmeans.com>
2015-03-27 12:19:25 -0600

    added files for npm in root

---
+++ config.rb
@@ -0,0 +1,10 @@
+# require "susy"
+# susy is a responsive grid - using DW's instead
+css_dir = '_css'
+sass_dir = 'components/sass'
+javascripts_dir = '_scripts'
+
+output_style = :expanded
+# output_style = :compressed
+
+#relative_assets = true;
